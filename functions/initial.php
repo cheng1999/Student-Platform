6609408ad5482423d0016c884fae2706a638afe3
Lee Guo Cheng <linuxer17@gmail.com>
2015-03-08 15:25:18 +0800

    work in lower version php

--- functions/initial.php
+++ functions/initial.php
@@ -8,10 +8,10 @@ session_start();
 
 
 //connnect sql
-if(!@mysql_connect("$DB_SERVER", "$DB_USER","$DB_PASS"))
+if(!@mysql_connect($DB_SERVER, $DB_USER,$DB_PASS))
         die("error while connect...");
-mysql_query("SET NAMES $DB_CHAR");
-if(!@mysql_select_db("$DB_NAME"))
+mysql_query("SET NAMES ".$DB_CHAR);
+if(!@mysql_select_db($DB_NAME))
         die("cannot use selected database");
 
 
